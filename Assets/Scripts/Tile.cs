509c07d3ac80b2b3dc27b2191ae30c616dd194f1
Philon <therealphilon@gmail.com>
2014-04-05 23:00:56 +0200

    skalowanie przeszkod.

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -22,29 +22,32 @@ public class Tile : MonoBehaviour
 	}
 
 	public void InitMe(float wallChance, float holeChance, GameObject parent, int i, int inGameY){
-
+		TileContent = TileContent.NONE;
 		float sumOfChances = wallChance + holeChance;
 		float ticket = Random.Range(0f, 1f);
 		Debug.Log ("ticket was: " + ticket );
+		SpriteRenderer r = null;
 		if (ticket < wallChance ){
 			TileContent = TileContent.WALL;
-			SpriteRenderer r = gameObject.AddComponent<SpriteRenderer>();
+			r = gameObject.AddComponent<SpriteRenderer>();
 			r.sprite = Resources.Load<Sprite>("Images/wall");
-			r.sortingLayerName = "Layer2";
 			Debug.Log("created wall");
 		} else if (ticket < wallChance + holeChance){
 			TileContent = TileContent.HOLE;
-			SpriteRenderer r = gameObject.AddComponent<SpriteRenderer>();
+			r = gameObject.AddComponent<SpriteRenderer>();
 			r.sprite = Resources.Load<Sprite>("Images/hole");
-			r.sortingLayerName = "Layer2";
 			Debug.Log("created hole");
 		}
-		transform.parent = parent.transform;
 		transform.localScale = new Vector3(1, 1, 1);
+		transform.parent = parent.transform;
+		if (r != null){
+			r.sortingLayerName = "Layer2";
+			float scale = InGamePosition.tileW/(float) r.sprite.bounds.size.x ;
+			transform.localScale = new Vector3(scale, scale, 0);
+		}
 		InGamePosition igp = gameObject.AddComponent<InGamePosition>();
 		igp.x = i;
 		igp.y = inGameY;
-
 	}
 	
 
