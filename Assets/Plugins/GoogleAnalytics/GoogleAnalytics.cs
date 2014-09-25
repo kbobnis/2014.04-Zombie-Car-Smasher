cb431a9028e5a0227887c0f6561fe3a7d8617d56
Philon <therealphilon@gmail.com>
2014-09-25 12:29:08 +0200

    removed READ_PHONE_STATE_PERMISSION

--- Assets/Plugins/GoogleAnalytics/GoogleAnalytics.cs
+++ Assets/Plugins/GoogleAnalytics/GoogleAnalytics.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System;
+
 
 public class GoogleAnalytics : MonoBehaviour {
 	
@@ -25,42 +27,35 @@ public class GoogleAnalytics : MonoBehaviour {
 			instance = this;
 		}
 	}
+
+	public static string GetUniqueID(){
+		string key = "uniqueId";
+		
+		string uniqueId = "";
+		if(PlayerPrefs.HasKey(key)){
+			uniqueId = PlayerPrefs.GetString(key);
+		} else {
+			uniqueId = System.Guid.NewGuid ().ToString();
+			PlayerPrefs.SetString(key, uniqueId);
+			PlayerPrefs.Save();
+		}
+
+		Debug.Log("Generated Unique ID: "+uniqueId);
+		
+		return uniqueId;
+	}
 	
 	void Start() 
 	{
-	
 		screenRes = Screen.width + "x" + Screen.height;
-		
-		clientID = SystemInfo.deviceUniqueIdentifier;
-			
+		clientID = GetUniqueID ();
 	}
 
 	public void LogScreen(string title)
 	{
-		
 		title = WWW.EscapeURL(title);
-		
 		var url = "http://www.google-analytics.com/collect?v=1&ul=en-us&t=appview&sr="+screenRes+"&an="+WWW.EscapeURL(appName)+"&a=448166238&tid="+propertyID+"&aid="+bundleID+"&cid="+WWW.EscapeURL(clientID)+"&_u=.sB&av="+appVersion+"&_v=ma1b3&cd="+title+"&qt=2500&z=185";
-		
 		WWW request = new WWW(url);
-		
-		/*if(request.error == null)
-		{
-			if (request.responseHeaders.ContainsKey("STATUS"))
-			{
-				if (request.responseHeaders["STATUS"] == "HTTP/1.1 200 OK")	
-				{
-					Debug.Log ("GA Success");
-				}else{
-					Debug.LogWarning(request.responseHeaders["STATUS"]);	
-				}
-			}else{
-				Debug.LogWarning("Event failed to send to Google");	
-			}
-		}else{
-			Debug.LogWarning(request.error.ToString());	
-		}*/
-		
 	}
 	
 	
